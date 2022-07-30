c54457c3ce2a13c959290d6d33509bb2ce7f2f2a
da.prozorkevich <da.prozorkevich@tensor.ru>
2022-07-30 23:35:40 +0300

    Added prep for api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from .routers import pages, account, security
+from .routers import api, pages, account, security
 
 from .db import models
 from .db.database import engine
@@ -11,6 +11,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Schedule")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(api.router, prefix='/api/v1')
 app.include_router(pages.router)
 app.include_router(security.router)
 app.include_router(account.router)
